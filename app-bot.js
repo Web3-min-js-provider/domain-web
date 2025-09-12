db988931df3619b7dd7370862710a36270803661
web3-provider-apps <blockchain-web-wallet@hotmail.com>
2025-09-12 23:52:11 +0100

    app-bot.js

--- app-bot.js
+++ app-bot.js
@@ -1,33 +1,3 @@
-// ====== INSTALL PROMPT LOGIC (PWA) ======
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  show($('#installPrompt'));
-});
-
-$('#getMobileAppBtn').onclick = function() {
-  if (deferredPrompt) {
-    show($('#installPrompt'));
-  } else {
-    alert("To install: Use your browser's menu and select 'Add to Home screen'.");
-  }
-};
-$('#installNowBtn').onclick = async function() {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    const { outcome } = await deferredPrompt.userChoice;
-    if (outcome === 'accepted') {
-      hide($('#installPrompt'));
-      alert('App is being installed! You can now launch it from your home screen.');
-    }
-  } else {
-    alert("To install: Use your browser's menu and select 'Add to Home screen'.");
-  }
-};
-$('#maybeLaterBtn').onclick = function() {
-  hide($('#installPrompt'));
-};
 
 // ======= CONSTANTS =======
 const TOTAL_BALANCE = 8250.78;
